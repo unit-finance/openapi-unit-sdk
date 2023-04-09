1f297a4a0ac01b0e82d44326ec31eeefc76ab251
axshani <aalexshani@gmail.com>
2023-04-09 19:17:10 +0300

    split openapi file to paths jsons c update generate script to remove ./schemas/ relative path next: add formatter to json files

--- generate.py
+++ generate.py
@@ -1,5 +1,21 @@
 import os
 
+path_of_the_directory = './schemas/'
+
+for filename in os.listdir(path_of_the_directory):
+    f_path = os.path.join(path_of_the_directory, filename)
+    if os.path.isfile(f_path):
+        f = open(f_path, 'r', encoding="utf8")
+        lineList = f.readlines()
+        f.close
+
+        # Re-open file here
+        f2 = open(f_path, 'w', encoding="utf8")
+        for line in lineList:
+            line = line.replace('./schemas/', '')
+            f2.write(line)
+        f2.close()
+
 os.system("java -jar swagger-codegen-cli-3.0.36.jar generate -i openapi.json -l python -o unit")
 
 path_of_the_directory = './unit/swagger_client/models/'
@@ -17,7 +33,6 @@ for filename in os.listdir(path_of_the_directory):
             line = line.replace('#/components/schemas/', '')
             f2.write(line)
         f2.close()
-
 
 f_path = "unit/swagger_client/api/create_application_api.py"
 f = open(f_path, 'r')

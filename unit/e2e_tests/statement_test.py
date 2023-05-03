fd95c1382cd66669b7a5dcfd374892f3518d78a9
axshani <aalexshani@gmail.com>
2023-05-03 11:40:02 +0300

    small changes update schema

--- unit/e2e_tests/statement_test.py
+++ unit/e2e_tests/statement_test.py
@@ -1,28 +1,33 @@
-import os
 import unittest
-from unit import Unit
-from unit.models.statement import GetStatementParams, ListStatementParams
 
-token = os.environ.get('TOKEN')
-client = Unit("https://api.s.unit.sh", token)
+from e2e_tests.helpers.helpers import create_api_client
+from swagger_client import GetListStatementsApi, GetStatementPDFApi, GetStatementHTMLApi
 
-def test_list_and_get_statements():
-    statements = client.statements.list(ListStatementParams(2)).data
-    for s in statements:
-        assert s.type == "accountStatementDTO"
 
-        params = GetStatementParams(s.id)
-        html_statement = client.statements.get(params).data
-        assert "<!DOCTYPE html>" in html_statement
+class TestStatementApi(unittest.TestCase):
+    """StatementAPI unit test stubs"""
 
-        params = GetStatementParams(s.id, customer_id=s.relationships["customer"].id)
-        html_statement = client.statements.get(params).data
-        assert "<!DOCTYPE html>" in html_statement
+    def setUp(self):
+        self.api_client = create_api_client()
 
-        account_id = s.relationships["account"].id
-        pdf_response = client.statements.get_bank_verification(account_id).data
-        assert "PDF" in pdf_response
+    def tearDown(self):
+        pass
 
-        params = GetStatementParams(s.id, "pdf")
-        pdf_statement = client.statements.get(params).data
-        assert "PDF" in pdf_statement
+    def test_list_and_get_statements(self):
+        statements = GetListStatementsApi(self.api_client).execute().data
+
+        for s in statements:
+            assert s.type == "accountStatementDTO"
+
+            pdf_statement = GetStatementPDFApi(self.api_client).execute(s.id)
+            assert "PDF" in pdf_statement
+
+            # account_id = s.relationships["account"].id
+            # pdf_response = client.statements.get_bank_verification(account_id).data
+
+            html_statement = GetStatementHTMLApi(self.api_client).execute(s.id)
+            assert "<!DOCTYPE html>" in html_statement
+
+    if __name__ == '__main__':
+        unittest.main()
+

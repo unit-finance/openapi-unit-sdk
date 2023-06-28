800c9719ce98e38cf0a9bc2614383e9fdaeb0fc9
axshani <aalexshani@gmail.com>
2023-06-28 19:03:25 +0300

    fix create webhook with test

--- unit/e2e_tests/webhook_test.py
+++ unit/e2e_tests/webhook_test.py
@@ -1,7 +1,7 @@
 import unittest
 
 from e2e_tests.helpers.helpers import create_api_client
-from swagger_client import GetListWebhooksApi
+from swagger_client import GetListWebhooksApi, CreateWebhookApi
 
 
 class TestWebhookApi(unittest.TestCase):
@@ -17,3 +17,18 @@ class TestWebhookApi(unittest.TestCase):
         response = GetListWebhooksApi(self.api_client).execute()
         for w in response.data:
             assert w.type == "webhook"
+
+    def test_create_webhook(self):
+        response = CreateWebhookApi(self.api_client).execute({"data": {"type": "webhook",
+            "attributes": {
+                "label": "some label",
+                "url": "https://webhook.site/81ee6b53-fde4-4b7d-85a0-0b6249a4488d",
+                "token": "MyToken",
+                "contentType": "Json",
+                "deliveryMode": "AtLeastOnce",
+                "includeResources": False,
+                "subscriptionType": "OnlyAuthorizationRequest"
+            }}})
+
+        assert response.data.type == "webhook"
+

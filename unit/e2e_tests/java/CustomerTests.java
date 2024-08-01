6cdfd50a30695c549b9c887d82ba79e075ceb0c2
Yehor Zhukovskyi <43119306+YegorZh@users.noreply.github.com>
2024-08-01 18:06:34 +0300

    Major refactor update, version bump to 0.1.0 (#90)
    * feat: updated repaymnet requests and responses
    
    * feat: one of repayment requests
    
    * feat: script to refactor schema files
    
    * temp: backup before another refactor script
    
    * feat: migrated schemas to 3.1, configured bundling
    
    * fix: downgraded to 3.0 for better compatability with generators, fixed improper oneOf uses for makign types nullable
    
    * fix: deprecated improper tag usage, now all paths define the 'unit' tag
    
    * feat: assigned proper operationIds
    
    * feat: added prettier into the project
    
    * feat: project structure cleanup
    
    * feat: updated tests, refactored application document upload path, readme
    
    * feat: renaming refactor
    
    * feat: updated operation id and titles, final fixes to relationships
    
    * Node generation and stop papyment updates (#5)
    
    * Transaction relationships (#2)
    
    * feat: updated relationships inside of transactions, fixed reversal related transaction types
    
    * fix: deprecated bill payment,  added temp workaround for undocumented relationships
    
    * feat: updated java tests to properly work with the refactor, fixed payment type mismatches, updated naming related to the patch requests
    
    * feat: final update to transactions
    
    * feat: node js generation and stop payment updates
    
    * feat: recurring repayments (#6)
    
    * Fixes and first batch of test updates (#7)
    
    * temp: repayment info and create fee fixs
    
    * fix: fixes to discriminator related declarations
    
    * fix: type enum proper typing
    
    * feat: test updates
    
    * Removal of trust features and bill payment  (#8)
    
    * feat: deprecated bill payment and trust application related functionality, removed schemas with oneOf
    
    * feat: version bump
    
    * feat: update for the lint workflow to use the root config
    
    * fix: small typo

--- unit/e2e_tests/java/CustomerTests.java
+++ unit/e2e_tests/java/CustomerTests.java
@@ -1,49 +1,130 @@
-package org.openapitools.client;
+package unit.java.sdk;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.openapitools.client.api.CreateApplicationApi;
-import org.openapitools.client.api.GetCustomerApi;
-import org.openapitools.client.api.GetListCustomersApi;
-import org.openapitools.client.model.IndividualApplication;
-import org.openapitools.client.model.IndividualCustomer;
-import org.openapitools.client.model.UnitCreateApplicationResponse;
-import org.openapitools.client.model.UnitCustomersListResponse;
+import org.junit.Test;
 
-import static org.openapitools.client.TestHelpers.CreateApplicationRequest;
+import unit.java.sdk.api.UnitApi;
+import unit.java.sdk.model.Application;
+import unit.java.sdk.model.ArchiveCustomerRequest;
+import unit.java.sdk.model.ArchiveCustomerRequestData;
+import unit.java.sdk.model.ArchiveCustomerRequestDataAttributes;
+import unit.java.sdk.model.BusinessApplication;
+import unit.java.sdk.model.BusinessCustomer;
+import unit.java.sdk.model.Contact;
+import unit.java.sdk.model.Customer;
+import unit.java.sdk.model.IndividualApplication;
+import unit.java.sdk.model.IndividualCustomer;
+import unit.java.sdk.model.ResponseContact;
+import unit.java.sdk.model.UnitCreateApplicationResponse;
+import unit.java.sdk.model.UnitCustomerResponse;
+import unit.java.sdk.model.UnitCustomersListResponse;
+import unit.java.sdk.model.UpdateBusinessCustomer;
+import unit.java.sdk.model.UpdateBusinessCustomerAttributes;
+import unit.java.sdk.model.UpdateCustomerRequest;
+import unit.java.sdk.model.UpdateCustomerRequestData;
+import unit.java.sdk.model.UpdateIndividualCustomer;
+import unit.java.sdk.model.UpdateIndividualCustomerAttributes;
+
+import static unit.java.sdk.TestHelpers.GenerateCreateApplicationRequest;
+import static unit.java.sdk.TestHelpers.GenerateCreateBusinessApplicationRequest;
+import static unit.java.sdk.TestHelpers.GenerateUnitApiClient;
+
 
 public class CustomerTests {
-    @BeforeAll
-    static void init() {
-        String access_token = System.getenv("access_token");
-        ApiClient cl = new ApiClient();
-        cl.setBearerToken(access_token);
-        Configuration.setDefaultApiClient(cl);
+    UnitApi unitApi = GenerateUnitApiClient();
+
+    public static IndividualCustomer CreateIndividualCustomer(UnitApi unitApi) throws ApiException {
+        UnitCreateApplicationResponse res = unitApi.createApplication(GenerateCreateApplicationRequest());
+        assert res.getData().getType().equals(Application.TypeEnum.INDIVIDUALAPPLICATION);
+
+        IndividualApplication app = (IndividualApplication) res.getData();
+        String customerId = app.getRelationships().getCustomer().getData().getId();
+
+        return (IndividualCustomer) unitApi.getCustomer(customerId).getData();
+    }
+
+    public static BusinessCustomer CreateBusinessCustomer(UnitApi unitApi) throws ApiException {
+        UnitCreateApplicationResponse res = unitApi.createApplication(GenerateCreateBusinessApplicationRequest());
+        assert res.getData().getType().equals(Application.TypeEnum.BUSINESSAPPLICATION);
+
+        BusinessApplication app = (BusinessApplication) res.getData();
+        String customerId = app.getRelationships().getCustomer().getData().getId();
+
+        return (BusinessCustomer) unitApi.getCustomer(customerId).getData();
     }
 
     @Test
     public void GetCustomersListApiTest() throws ApiException {
-        GetListCustomersApi api = new GetListCustomersApi();
-
-        UnitCustomersListResponse response = api.execute(null, null, null);
-        assert response.getData().size() > 0;
-    }
-
-    public static IndividualCustomer CreateIndividualCustomer() throws ApiException {
-        CreateApplicationApi apiClient = new CreateApplicationApi();
-        UnitCreateApplicationResponse res = apiClient.execute(CreateApplicationRequest());
-        assert res.getData().getType().equals("individualApplication");
-
-        IndividualApplication app = (IndividualApplication) res.getData();
-        String customerId = app.getRelationships().getCustomer().getData().getId();
-        GetCustomerApi customerApi = new GetCustomerApi();
-
-        return (IndividualCustomer) customerApi.execute(customerId).getData();
+        UnitCustomersListResponse response = unitApi.getCustomersList(null, null, null);
+        assert !response.getData().isEmpty();
     }
 
     @Test
     public void CreateIndividualCustomerTest() throws ApiException {
-        IndividualCustomer customer = CreateIndividualCustomer();
-        assert customer.getType().equals("individualCustomer");
+        IndividualCustomer customer = CreateIndividualCustomer(unitApi);
+        assert customer.getType().equals(IndividualCustomer.TypeEnum.INDIVIDUALCUSTOMER);
     }
-}
+
+    @Test
+    public void UpdateIndividualCustomerTest() throws ApiException {
+        IndividualCustomer customer = CreateIndividualCustomer(unitApi);
+        assert customer.getType().equals(IndividualCustomer.TypeEnum.INDIVIDUALCUSTOMER);
+
+        String email = "test@mail.test";
+        UpdateIndividualCustomer updateIndividualCustomer = new UpdateIndividualCustomer();
+        UpdateIndividualCustomerAttributes updateIndividualCustomerAttributes = new UpdateIndividualCustomerAttributes();
+        updateIndividualCustomerAttributes.email(email);
+        updateIndividualCustomer.setAttributes(updateIndividualCustomerAttributes);
+        updateIndividualCustomer.setType(UpdateIndividualCustomer.TypeEnum.INDIVIDUALCUSTOMER);
+        UpdateCustomerRequest updateCustomerRequest = new UpdateCustomerRequest();
+        UpdateCustomerRequestData updateCustomerRequestData = new UpdateCustomerRequestData(updateIndividualCustomer);
+        updateCustomerRequest.setData(updateCustomerRequestData);
+
+        UnitCustomerResponse updateCutomerResponse = unitApi.updateCustomer(customer.getId(), updateCustomerRequest);
+        IndividualCustomer individualCustomer =  (IndividualCustomer) updateCutomerResponse.getData();
+        assert individualCustomer.getType().equals(IndividualCustomer.TypeEnum.INDIVIDUALCUSTOMER);
+        assert individualCustomer.getAttributes().getEmail().equals(email);
+    }
+
+    @Test 
+    public void UpdateBusinessCustomerTest() throws ApiException {
+        BusinessCustomer customer = CreateBusinessCustomer(unitApi);
+        assert customer.getType().equals(IndividualCustomer.TypeEnum.BUSINESSCUSTOMER);
+
+        String email = "test@mail.test";
+        UpdateBusinessCustomer updateBusinessCustomer = new UpdateBusinessCustomer();
+        UpdateBusinessCustomerAttributes updateBusinessCustomerAttributes = new UpdateBusinessCustomerAttributes();
+        Contact contact = customer.getAttributes().getContact();
+        ResponseContact responseContact = new ResponseContact();
+        responseContact.setEmail(email);
+        responseContact.setFullName(contact.getFullName());
+        responseContact.setPhone(contact.getPhone());
+        responseContact.setJwtSubject(contact.getJwtSubject());
+        updateBusinessCustomerAttributes.contact(responseContact);
+        updateBusinessCustomer.setAttributes(updateBusinessCustomerAttributes);
+        updateBusinessCustomer.setType(UpdateBusinessCustomer.TypeEnum.BUSINESSCUSTOMER);
+        UpdateCustomerRequest updateCustomerRequest = new UpdateCustomerRequest();
+        UpdateCustomerRequestData updateCustomerRequestData = new UpdateCustomerRequestData(updateBusinessCustomer);
+        updateCustomerRequest.setData(updateCustomerRequestData);
+
+        UnitCustomerResponse updateCutomerResponse = unitApi.updateCustomer(customer.getId(), updateCustomerRequest);
+        BusinessCustomer individualCustomer =  (BusinessCustomer) updateCutomerResponse.getData();
+        assert individualCustomer.getType().equals(BusinessCustomer.TypeEnum.BUSINESSCUSTOMER);
+        assert individualCustomer.getAttributes().getContact().getEmail().equals(email);
+    }
+
+    @Test 
+    public void ArchiveCustomerTest() throws ApiException {
+        Customer customer = CreateBusinessCustomer(unitApi);
+        assert customer.getType().equals(IndividualCustomer.TypeEnum.BUSINESSCUSTOMER);
+
+        ArchiveCustomerRequest request = new ArchiveCustomerRequest();
+        ArchiveCustomerRequestData requestData = new ArchiveCustomerRequestData();
+        ArchiveCustomerRequestDataAttributes requestDataAttributes = new ArchiveCustomerRequestDataAttributes();
+        requestDataAttributes.setReason(unit.java.sdk.model.ArchiveCustomerRequestDataAttributes.ReasonEnum.INACTIVE);
+        requestData.setAttributes(requestDataAttributes);
+        requestData.setType(ArchiveCustomerRequestData.TypeEnum.ARCHIVECUSTOMER);
+        request.setData(requestData);
+        UnitCustomerResponse res = unitApi.archiveCustomer(customer.getId(), request);
+        assert res.getData().getType().equals(Customer.TypeEnum.BUSINESSCUSTOMER);
+    }
+ }
--- unit/e2e_tests/java/TestHelpers.java
+++ unit/e2e_tests/java/TestHelpers.java
@@ -1,20 +1,53 @@
-package org.openapitools.client;
-
-import org.openapitools.client.model.*;
+package unit.java.sdk;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import unit.java.sdk.api.UnitApi;
+import unit.java.sdk.model.Address;
+import unit.java.sdk.model.AnnualIncome;
+import unit.java.sdk.model.BusinessAnnualRevenue;
+import unit.java.sdk.model.BusinessNumberOfEmployees;
+import unit.java.sdk.model.BusinessVertical;
+import unit.java.sdk.model.CashFlow;
+import unit.java.sdk.model.Contact;
+import unit.java.sdk.model.CreateApplicationRequest;
+import unit.java.sdk.model.CreateApplicationRequestData;
+import unit.java.sdk.model.CreateBusinessApplication;
+import unit.java.sdk.model.CreateBusinessApplicationAttributes;
+import unit.java.sdk.model.CreateOfficer;
+import unit.java.sdk.model.EntityType;
+import unit.java.sdk.model.FullName;
+import unit.java.sdk.model.Occupation;
+import unit.java.sdk.model.PaymentCounterparty;
+import unit.java.sdk.model.Phone;
+import unit.java.sdk.model.SourceOfIncome;
+import unit.java.sdk.model.WirePaymentCounterparty;
 
 public class TestHelpers {
-    public static CreateApplication CreateApplicationRequest() {
-        CreateIndividualApplication createIndividualApplication = new CreateIndividualApplication();
-        CreateIndividualApplicationAttributes attr = new CreateIndividualApplicationAttributes();
+    private static UnitApi unitApi;
+    static UnitApi GenerateUnitApiClient() {
+        if(unitApi == null){
+            String access_token = System.getenv("access_token");
+        ApiClient cl = new ApiClient();
+        cl.setRequestInterceptor(r -> r.header("Authorization", "Bearer " + access_token));
+        unitApi = new UnitApi(cl);
+        }
 
-        FullName fn = new FullName();
+        return unitApi;
+    }
+
+    public static CreateApplicationRequest GenerateCreateApplicationRequest() {
+        unit.java.sdk.model.CreateIndividualApplication createIndividualApplication = new unit.java.sdk.model.CreateIndividualApplication();
+        unit.java.sdk.model.CreateIndividualApplicationAttributes attr = new unit.java.sdk.model.CreateIndividualApplicationAttributes();
+
+        unit.java.sdk.model.FullName fn = new unit.java.sdk.model.FullName();
         fn.setFirst("Peter");
         fn.setLast("Parker");
         attr.setFullName(fn);
 
-        Address address = new Address();
+        unit.java.sdk.model.Address address = new unit.java.sdk.model.Address();
         address.setStreet("20 Ingram St");
         address.setCity("Forest Hills");
         address.setPostalCode("11375");
@@ -25,18 +58,120 @@ public class TestHelpers {
         attr.setSsn("721074426");
         attr.setDateOfBirth(LocalDate.parse("2001-08-10"));
         attr.setEmail("peter@oscorp.com");
-        Phone p = new Phone();
+        unit.java.sdk.model.Phone p = new unit.java.sdk.model.Phone();
         p.setNumber("5555555555");
         p.setCountryCode("1");
         attr.setPhone(p);
         attr.setIdempotencyKey("3a1a33be-4e12-4603-9ed0-820922389fb8");
-        attr.setOccupation(Occupation.ARCHITECTORENGINEER);
+        attr.setOccupation(unit.java.sdk.model.Occupation.ARCHITECTORENGINEER);
 
         createIndividualApplication.setAttributes(attr);
 
-        CreateApplication ca = new CreateApplication();
-        ca.data(new CreateApplicationData(createIndividualApplication));
+        CreateApplicationRequest ca = new CreateApplicationRequest();
+        ca.data(new CreateApplicationRequestData(createIndividualApplication));
 
         return ca;
+    }
+
+    public static CreateApplicationRequest GenerateCreateBusinessApplicationRequest() {
+        CreateBusinessApplication createBusinessApplication = new CreateBusinessApplication();
+        CreateBusinessApplicationAttributes attr = new CreateBusinessApplicationAttributes();
+
+        attr.setName("Peter Parker");
+
+        Address address = new Address();
+        address.setStreet("20 Ingram St");
+        address.setCity("Forest Hills");
+        address.setPostalCode("11375");
+        address.setCountry("US");
+        address.setState("NY");
+        attr.setAddress(address);
+
+        
+        Phone p = new Phone();
+        p.setNumber("5555555555");
+        p.setCountryCode("1");
+        attr.setPhone(p);
+
+        attr.setStateOfIncorporation("DE");
+        attr.setEin("123456789");
+        attr.setEntityType(EntityType.CORPORATION);
+        attr.setIp("127.0.0.1");
+        attr.setAnnualRevenue(BusinessAnnualRevenue.BETWEEN250KAND500K);
+        attr.setNumberOfEmployees(BusinessNumberOfEmployees.BETWEEN100AND500);
+        attr.setCashFlow(CashFlow.PREDICTABLE);
+        attr.setYearOfIncorporation("1999");
+        List<String> countriesOfOperation = new ArrayList<String>();
+        countriesOfOperation.add("US");
+        countriesOfOperation.add("CA");
+
+        attr.setCountriesOfOperation(countriesOfOperation);
+
+        attr.setWebsite(null);
+        
+        String email = "richard@piedpiper.com";
+        Contact contact = new Contact();
+        contact.setEmail(email);
+        contact.setPhone(p);
+        FullName fn = new FullName();
+        fn.setFirst("Peter");
+        fn.setLast("Parker");
+        contact.setFullName(fn);
+        attr.setContact(contact);
+
+
+        CreateOfficer officer = new CreateOfficer();
+        officer.setAnnualIncome(AnnualIncome.BETWEEN50KAND100K);
+        officer.setFullName(fn);
+        officer.setAddress(address);
+        officer.setEmail(email);
+        officer.setPhone(p);
+        LocalDate dateOfBirh = LocalDate.of(1997, 11, 1);
+        officer.setDateOfBirth(dateOfBirh);
+        officer.setTitle(CreateOfficer.TitleEnum.CEO);
+        officer.setOccupation(Occupation.ARCHITECTORENGINEER);
+        officer.setSourceOfIncome(SourceOfIncome.BUSINESSOWNERSHIPINTERESTS);
+        officer.setSsn("721074426");
+
+        attr.setOfficer(officer);
+        attr.setBusinessVertical(BusinessVertical.ARTSENTERTAINMENTANDRECREATION);
+
+        createBusinessApplication.setAttributes(attr);
+
+        CreateApplicationRequest ca = new CreateApplicationRequest();
+        ca.data(new CreateApplicationRequestData(createBusinessApplication));
+
+        return ca;
+    }
+
+    public static PaymentCounterparty CreatePaymentCounterparty() {
+        PaymentCounterparty counterparty = new PaymentCounterparty();
+        counterparty.setName("Jane Doe");
+        counterparty.setRoutingNumber("812345673");
+        counterparty.setAccountNumber("12345569");
+        counterparty.setAccountType(PaymentCounterparty.AccountTypeEnum.CHECKING);
+
+        return counterparty;
+    }
+
+    public static Address CreateAddress() {
+        Address address = new Address();
+        address.setStreet("20 Ingram St");
+        address.setCity("Forest Hills");
+        address.setState("CA");
+        address.setPostalCode("11375");
+        address.setCountry("US");
+
+        return address;
+    }
+
+    public static WirePaymentCounterparty CreateWirePaymentCounterparty() {
+        WirePaymentCounterparty counterparty = new WirePaymentCounterparty();
+        counterparty.setName("April Oniel");
+        counterparty.setAccountNumber("1000000001");
+        counterparty.setRoutingNumber("812345678");
+        counterparty.setAddress(CreateAddress());
+
+        return counterparty;
     }
 }

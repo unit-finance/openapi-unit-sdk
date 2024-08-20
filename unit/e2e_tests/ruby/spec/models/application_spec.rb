5f34b290b1d3a812ebf1ba0dc57ac9e40b14c969
stymoshchuk <sol.tymoshchuk@gmail.com>
2024-08-20 11:38:33 +0200

    RB-test-new-openapi_YH update tests

--- unit/e2e_tests/ruby/spec/models/application_spec.rb
+++ unit/e2e_tests/ruby/spec/models/application_spec.rb
@@ -7,40 +7,25 @@ require 'date'
 
 RSpec.describe 'Application' do
   before do
-    # run before each test
     configure_tests
   end
 
   describe 'test attribute "id"' do
-    let(:client) { OpenapiClient::ApiClient.new(configuration) }
+    let(:api_instance) { OpenapiClient::UnitApi.new(OpenapiClient::ApiClient.new(configuration)) }
 
     it 'should create an application' do
-      api_instance = OpenapiClient::CreateApplicationApi.new(client)
       request = { data: OpenapiClient::CreateIndividualApplication.new(type: 'individualApplication',
                                                                        attributes: OpenapiClient::CreateIndividualApplicationAttributes.new(
                                                                          ssn: '123456789', full_name: OpenapiClient::FullName.new({ first: 'John', last: 'Kenn' }), date_of_birth: Date.new(1989, 2, 1), address: ADDRESS, email: EMAIL, phone: PHONE,
                                                                          occupation: 'ArchitectOrEngineer', annual_income: 'UpTo10k', source_of_income: 'EmploymentOrPayrollIncome'
                                                                        )).to_hash }
-      response = api_instance.execute(request)
+      response = api_instance.create_application(request)
       expect(response.data.type).to eq('individualApplication')
     end
 
     it 'should upload a document for the application' do
-      api_instance = OpenapiClient::UploadAPDFDocumentForAnApplicationApi.new(client)
-      request = api_instance.execute('836683', '125214', get_document_contents)
+      request = api_instance.upload_application_document_file('2624618', '541996', get_document_contents)
 
-      expect(request.data.type).to eq('document')
-    end
-
-    it 'should upload a document for the application' do
-      api_instance = OpenapiClient::UploadAPNGDocumentForAnApplicationApi.new(client)
-      request = api_instance.execute('836683', '125215', get_image_contents('./spec/picture.png'))
-      expect(request.data.type).to eq('document')
-    end
-
-    it 'should upload a document for the application' do
-      api_instance = OpenapiClient::UploadAJPEGDocumentForAnApplicationApi.new(client)
-      request = api_instance.execute('836683', '125216', get_image_contents('./spec/check1.jpg'))
       expect(request.data.type).to eq('document')
     end
   end

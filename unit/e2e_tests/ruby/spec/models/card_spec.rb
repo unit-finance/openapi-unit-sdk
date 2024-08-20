76f97c98d828479a253f7c4c21ece0f806acaf5d
stymoshchuk <109137433+stymoshchuk@users.noreply.github.com>
2024-08-20 13:13:48 +0200

    Ruby tests update (#93)
    * RB-test-new-openapi_YH update tests
    
    * RB-test-new-openapi_YH update tests
    
    * RB-test-new-openapi_YH update statement_test
    
    * RB-tests-upd update createAccount.json

--- unit/e2e_tests/ruby/spec/models/card_spec.rb
+++ unit/e2e_tests/ruby/spec/models/card_spec.rb
@@ -9,31 +9,40 @@ RSpec.describe 'Card' do
   end
 
   describe 'test an instance of CreateCard' do
-    let(:api_instance) { OpenapiClient::CreateACardApi.new(OpenapiClient::ApiClient.new(configuration)) }
+    let(:api_instance) { OpenapiClient::UnitApi.new(OpenapiClient::ApiClient.new(configuration)) }
+    let(:account_for_business) { api_instance.create_account(
+      data: {
+        type: 'depositAccount',
+        attributes: OpenapiClient::CreateDepositAccountAttributes.new(
+          deposit_product: 'checking',
+          tags: { "purpose": 'checking' },
+          idempotency_key: '1234567890'
+        ).to_hash, relationships: RELATIONSHIPS_BUSINESS }).data.id}
+
 
     it 'should create an individual debit card' do
-      request = { data: OpenapiClient::CreateIndividualDebitCard.new(type: 'individualDebitCard', attributes:
-        OpenapiClient::CreateIndividualDebitCardAttributes.new(
+      request = { data: OpenapiClient::CreateIndividualDebitCardRequest.new(type: 'individualDebitCard', attributes:
+        OpenapiClient::CreateIndividualDebitCardRequestAttributes.new(
           { shipping_address: ADDRESS, tags: { "purpose": 'checking' },
-            additional_embossed_text: 'additional_text', expiry_date: '03/27' }
+            additional_embossed_text: 'additional_text', expiry_date: '03/28' }
         ),
-                                                                     relationships: OpenapiClient::CardRelationships.new(account: { "data": { "type": 'depositAccount',
-                                                                                                                                              "id": '2002413' } })).to_hash }
-      response = api_instance.execute(request)
+                                                                            relationships: OpenapiClient::CardRelationships.new(account: { "data": { "type": 'depositAccount',
+                                                                                                                                                     "id": '4009403' } })).to_hash }
+      response = api_instance.create_card(request)
       expect(response.data.type).to eq('individualDebitCard')
     end
 
     it 'should create a business debit card' do
-      request = { data: OpenapiClient::CreateBusinessDebitCard.new(type: 'businessDebitCard', attributes:
-        OpenapiClient::CreateBusinessDebitCardAttributes.new(
+      request = { data: OpenapiClient::CreateBusinessDebitCardRequest.new(type: 'businessDebitCard', attributes:
+        OpenapiClient::CreateBusinessDebitCardRequestAttributes.new(
           { shipping_address: ADDRESS, address: ADDRESS, full_name: FULL_NAME, phone: PHONE,
             email: EMAIL, date_of_birth: DATE_OF_BIRTH, nationality: 'US', ssn: SSN,
             tags: { "purpose": 'business' }, limits: LIMITS, idempotency_key: '1234567890',
-            print_only_business_name: false, expiry_date: '03/27' }
+            print_only_business_name: false, expiry_date: '03/28' }
         ),
-                                                                   relationships: OpenapiClient::CardRelationships.new(account: { "data": { "type": 'account',
-                                                                                                                                            "id": '1567861' } })).to_hash }
-      response = api_instance.execute(request)
+                                                                          relationships: OpenapiClient::CardRelationships.new(account: { "data": { "type": 'account',
+                                                                                                                                                   "id": '3969030' } })).to_hash }
+      response = api_instance.create_card(request)
       expect(response.data.type).to eq('businessDebitCard')
     end
   end

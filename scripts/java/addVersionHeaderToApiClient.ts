e5b3de18fb126e81d83e0b0d6ef7db6d117e66f3
YegorZh <yegorzhukovskiya@gmail.com>
2024-11-20 12:46:46 +0000

    feat: generation now also injects headers for setInterceptor function of ApiClient

--- scripts/java/addVersionHeaderToApiClient.ts
+++ scripts/java/addVersionHeaderToApiClient.ts
@@ -3,15 +3,25 @@ import fs from "fs";
 
 function addVersionHeadersToApiClient(data: string): string {
     const interceptorRegex = /interceptor = null;/gm;
-
-    var processedData = data;
-    console.log("API VERSION: " + process.env.API_VERSION);
     const interceptorWithVersionHeader = `interceptor = (req) -> {
       req.setHeader("X-UNIT-SDK", "unit-node-sdk@v${process.env.API_VERSION}");
     };`;
+
+    const setInterceptorRegex = /this\.interceptor = interceptor;/gm;
+    const setInterceptorWithVersionHeader = `this.interceptor = (req) -> {
+      req.setHeader("X-UNIT-SDK", "unit-node-sdk@v${process.env.API_VERSION}");
+      interceptor.accept(req);
+    };`;
+
+    var processedData = data;
     processedData = processedData.replaceAll(
         interceptorRegex,
         interceptorWithVersionHeader
+    );
+
+    processedData = processedData.replaceAll(
+        setInterceptorRegex,
+        setInterceptorWithVersionHeader
     );
 
     return processedData;

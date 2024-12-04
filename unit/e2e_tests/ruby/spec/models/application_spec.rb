78632de08e13f5d67a0ae399da11358f7aabe75d
YegorZh <yegorzhukovskiya@gmail.com>
2024-12-04 01:45:28 +0000

    feat: ruby test updates, small ruby and python generation updates, readme ruby usage example

--- unit/e2e_tests/ruby/spec/models/application_spec.rb
+++ unit/e2e_tests/ruby/spec/models/application_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'openapi_client'
+require 'unit_openapi_ruby_sdk'
 require_relative '../spec_helper'
 require 'json'
 require 'date'
@@ -11,22 +11,16 @@ RSpec.describe 'Application' do
   end
 
   describe 'test attribute "id"' do
-    let(:api_instance) { OpenapiClient::UnitApi.new(OpenapiClient::ApiClient.new(configuration)) }
+    let(:api_instance) { UnitOpenapiRubySdk::UnitApi.new(UnitOpenapiRubySdk::ApiClient.new(configuration)) }
 
     it 'should create an application' do
-      request = { data: OpenapiClient::CreateIndividualApplication.new(type: 'individualApplication',
-                                                                       attributes: OpenapiClient::CreateIndividualApplicationAttributes.new(
-                                                                         ssn: '123456789', full_name: OpenapiClient::FullName.new({ first: 'John', last: 'Kenn' }), date_of_birth: Date.new(1989, 2, 1), address: ADDRESS, email: EMAIL, phone: PHONE,
+      request = { data: UnitOpenapiRubySdk::CreateIndividualApplication.new(type: 'individualApplication',
+                                                                       attributes: UnitOpenapiRubySdk::CreateIndividualApplicationAttributes.new(
+                                                                         ssn: '123456789', full_name: UnitOpenapiRubySdk::FullName.new({ first: 'John', last: 'Kenn' }), date_of_birth: Date.new(1989, 2, 1), address: ADDRESS, email: EMAIL, phone: PHONE,
                                                                          occupation: 'ArchitectOrEngineer', annual_income: 'UpTo10k', source_of_income: 'EmploymentOrPayrollIncome'
                                                                        )).to_hash }
       response = api_instance.create_application(request)
       expect(response.data.type).to eq('individualApplication')
-    end
-
-    it 'should upload a document for the application' do
-      request = api_instance.upload_application_document_file('2624618', '541996', get_document_contents)
-
-      expect(request.data.type).to eq('document')
     end
   end
 end

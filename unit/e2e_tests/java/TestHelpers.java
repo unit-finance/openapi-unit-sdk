65f6b0559de73b96cd29b8c74cf4a7261ca91ed6
Yehor Zhukovskyi <43119306+YegorZh@users.noreply.github.com>
2025-03-11 08:40:47 +0000

    Updated finance related ints to be int64 (#104)

--- unit/e2e_tests/java/TestHelpers.java
+++ unit/e2e_tests/java/TestHelpers.java
@@ -1,10 +1,8 @@
 package unit.java.sdk;
 
-import java.net.http.HttpRequest;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -46,11 +44,8 @@ public class TestHelpers {
         ObjectMapper mapper = cl.getObjectMapper();
         mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         cl.setObjectMapper(mapper);
-        Consumer<HttpRequest.Builder> defaultInterceptor = cl.getRequestInterceptor();
         cl.setRequestInterceptor(r -> {
             r.header("Authorization", "Bearer " + access_token);
-            System.err.println(r.build().headers().toString());
-            System.err.println("println test");
         });
         unitApi = new UnitApi(cl);
         }
